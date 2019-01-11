89f2be8300a895a13eb49336bcb2421e7a58d8d6
zhaitao1992 <464330453@qq.com>
2019-01-11 15:07:23 +0800

    AutoADSL

---
+++ AutoASDL/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoASDL
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //刷新
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //拨号
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //断开
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            //保存账号
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            //IP池添加
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            //IP池删除
+        }
+    }
+}
